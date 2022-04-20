dffd88d5b56c3c5805f2f798d586072b76092949
Rohit Jagga <rohitjagga02@gmail.com>
2022-04-20 15:56:38 -0400

    Update MapScreen.js

--- mobile-app/MapScreen.js
+++ mobile-app/MapScreen.js
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet,
@@ -6,23 +6,39 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import MapView from 'react-native-maps';
 
 function MapScreen({ route, navigation }) {
-
-    const username = route.params;
+  const username = route.params;
 
   return (
-    <MapView        
-    style={{flex: 1}}        
-    region={{          
-        latitude: 39.9522,          
-        longitude: -75.1932,          
-        latitudeDelta: 0.3,          
-        longitudeDelta: 0.3 }}        
-        showsUserLocation={true} /> 
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: 39.9522,
+          longitude: -75.1932,
+          latitudeDelta: 0.3,
+          longitudeDelta: 0.3,
+        }}
+        showsUserLocation={true}
+      />
+    </View>
   );
 }
 export default MapScreen;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
