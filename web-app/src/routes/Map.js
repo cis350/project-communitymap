11ad01d3dc50e9eef11a98bead8a8e3bf1feadb5
ninatansey <51764676+ninatansey@users.noreply.github.com>
2022-04-26 15:18:22 -0400

    working iframe

--- web-app/src/routes/Map.js
+++ web-app/src/routes/Map.js
@@ -1,5 +1,6 @@
 import { Link, Outlet} from "react-router-dom";
 import '../App.css'
+import { googleMapLoader } from "google-map-react";
 
 export default function Map() {
     return (
@@ -16,6 +17,28 @@ export default function Map() {
           <Link to="/map">Map</Link>
         </nav>
         <h2>Map</h2>
+        {/* input zipcode / have from user to load map */}
+        {/* <div>
+          <iframe
+            width="600"
+            height="450"
+            style="border:0"
+            loading="lazy"
+            allowfullscreen
+            referrerpolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB_L2hlVtZrgal7H8RQKzMzhPZDB8AJj2A&q=Clark+Park,Philadelphia+PA">
+          </iframe>
+        </div> */}
+        
+        <p>Scroll around the map to explore points of interest near you!</p>
+        <div id="map" style={{width: "320px", height: "480px"}}> 
+        <iframe width="600" height="450" loading="lazy" allowfullscreen
+src="https://www.google.com/maps/embed/v1/view?zoom=15&center=39.9495,-75.1719&key=AIzaSyB_L2hlVtZrgal7H8RQKzMzhPZDB8AJj2A"></iframe>
+        </div>
+        <div>
+          {/* <input id="address" type="textbox" value="Phildelphia, PA"/>
+          <input type="button" value="Encode" onclick="codeAddress()"/> */}
+        </div>
       </main>
     );
   }
